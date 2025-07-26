c095684529b2510b0168e2416fa10f9be2893404
Athul Anoop <athulanoop2020@gmail.com>
2025-07-26 16:40:24 +0530

    add: prescriptions model and relations

--- apps/server/src/models/relations.js
+++ apps/server/src/models/relations.js
@@ -1,8 +1,47 @@
-import { User, AuthToken } from "#models";
+import {
+    User,
+    AuthToken,
+    Prescription,
+    Medication,
+    MedicationSchedule,
+} from "#models";
 import { DataTypes } from "sequelize";
 
 await User.hasOne(AuthToken, {
     foreignKey: "userId",
     type: DataTypes.UUID,
     unique: true,
+});
+
+// --- User <-> Prescription (One-to-Many) ---
+// A user can have many prescriptions.
+User.hasMany(Prescription, {
+    foreignKey: {
+        name: "userId",
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    as: "prescriptions", // Alias for when you query
+});
+// Each prescription belongs to a single user.
+Prescription.belongsTo(User, {
+    foreignKey: "userId",
+    as: "user",
+});
+
+// --- Prescription <-> Medication (One-to-Many) ---
+// A prescription can list many medications.
+Prescription.hasMany(Medication, {
+    foreignKey: {
+        name: "prescriptionId",
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    as: "medications",
+    onDelete: "CASCADE", // If a prescription is deleted, delete its medications
+});
+// Each medication is part of a single prescription.
+Medication.belongsToMany(Prescription, {
+    foreignKey: "prescriptionId",
+    as: "prescription",
 });
