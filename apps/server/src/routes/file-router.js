bb0fccb67806f60f9acedd1b190921fd1e95acea
Athul Anoop <athulanoop2020@gmail.com>
2025-07-26 18:10:20 +0530

    update: comment unfinished route path

--- apps/server/src/routes/file-router.js
+++ apps/server/src/routes/file-router.js
@@ -5,6 +5,6 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/", upload.single("file"), fileController.uploadFile);
-router.get("/:name", fileController.getFile);
+// router.get("/:name", fileController.getFile);
 
 export default router;
