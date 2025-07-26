caece6d98c2d1d47c03e861c1d3090be09be7dfc
sharon504 <sharonpshajan@gmail.com>
2025-07-26 12:10:18 +0530

    Update cors to include production and dev origin

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -8,7 +8,10 @@ export default function createApp() {
 
     app.use(
         cors({
-            origin: "*",
+            origin:
+                process.env.NODE_ENV === "production"
+                    ? "https://vitalis-client.vercel.app"
+                    : "http://localhost:3000",
             credentials: true,
             methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             allowedHeaders: ["Content-Type", "Authorization"],
