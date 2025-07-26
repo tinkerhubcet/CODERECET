02452d5ab31a102c7168a3f4aecee3cdf55fa009
sharon504 <sharonpshajan@gmail.com>
2025-07-26 10:02:44 +0530

    Initializing models and moving routes to createApp

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { authRouter, userRouter } from "#routes";
 
 export default function createApp() {
     const app = express();
+    app.use("/auth", authRouter);
+    app.use("/user", userRouter);
 
     app.get("/", (req, res) => {
         res.send("Hello World!");
--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,17 +1,16 @@
 import createApp from "./app.js";
 import dotenv from "dotenv";
 import { connect } from "#utils";
-import "#models";
-import { userRouter, authRouter } from "#routes";
+import { User, AuthToken } from "#models";
 
 dotenv.config({ path: ".env.dev" });
 
 const { PORT } = process.env;
 
-const app = createApp();
 connect();
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
+User.sync();
+AuthToken.sync();
+const app = createApp();
 
 app.listen({ port: PORT }, () => {
     console.log(`Server listening on port ${PORT}`);
